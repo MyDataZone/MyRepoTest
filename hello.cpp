b2ac08ebd21da674fa1b0fbb7865267ea9c6e9ea
John Anderson <52807551+NameLessZone@users.noreply.github.com>
2020-01-11 18:25:57 +0800

    Update hello.cpp
    cpp Test

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
 
 int main()
 {
   std::cout << "Hello Git" << std::endl;
+  int iarr[]{0,1,3,5};
+  vector<int> vec(iarr,iarr+sizeof(iarr)/sizeof(int));
+  for(vector<int>::iterator it = begin(vec);vec!=end(vec);i++)
+  {
+	  printf("Number %d is %d\n",distance(begin(vec),it),*it);
+  }
   return 0;
 }
